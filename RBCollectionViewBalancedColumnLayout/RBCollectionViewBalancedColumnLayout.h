fafca90e0000e5f716cf698b425aed0a08b4c3cc
Rob Booth <rob.o.booth@gmail.com>
2014-02-28 14:28:25 -0800

    Adding some comments for clarity.

--- RBCollectionViewBalancedColumnLayout/RBCollectionViewBalancedColumnLayout.h
+++ RBCollectionViewBalancedColumnLayout/RBCollectionViewBalancedColumnLayout.h
@@ -14,16 +14,53 @@ FOUNDATION_EXPORT NSString *const RBCollectionViewBalancedColumnFooterKind;
 
 @protocol RBCollectionViewBalancedColumnLayoutDelegate <NSObject>
 
+/**
+ *  Height of the cell for indexPath, cells will be fit to the global cellWidth set on the layout
+ *
+ *  @param collectionView       target collection view
+ *  @param collectionViewLayout reference to layout
+ *  @param indexPath            cell index path
+ *
+ *  @return the height of the cell
+ */
 - (CGFloat)collectionView:(UICollectionView *)collectionView layout:(RBCollectionViewBalancedColumnLayout *)collectionViewLayout heightForItemAtIndexPath:(NSIndexPath *)indexPath;
 
 @optional
+
+/**
+ *  Height of the header for a section (0 to disable it)
+ *
+ *  @param collectionView       target collection view
+ *  @param collectionViewLayout reference to layout
+ *  @param indexPath            section index path
+ *
+ *  @return the height of the header
+ */
 - (CGFloat)collectionView:(UICollectionView *)collectionView layout:(RBCollectionViewBalancedColumnLayout *)collectionViewLayout heightForHeaderInSection:(NSInteger)section;
+
+/**
+ *  Height of the footer for a section (0 to disable it)
+ *
+ *  @param collectionView       target collection view
+ *  @param collectionViewLayout reference to layout
+ *  @param indexPath            section index path
+ *
+ *  @return the height of the footer
+ */
 - (CGFloat)collectionView:(UICollectionView *)collectionView layout:(RBCollectionViewBalancedColumnLayout *)collectionViewLayout heightForFooterInSection:(NSInteger)section;
+
 @end
 
 @interface RBCollectionViewBalancedColumnLayout : UICollectionViewLayout
 
+/**
+ * Width for the cells
+ */
 @property (nonatomic, assign) NSUInteger cellWidth;
+
+/**
+ * Vertical spaceing between cells
+ */
 @property (nonatomic, assign) CGFloat interItemSpacingY;
 
 @end
